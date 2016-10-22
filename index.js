1247728906311d3272da0ff012c63e9d0bb7a2aa
Sean Wilson <hello@imsean.me>
2016-10-21 19:33:34 -0500

    Fixed prefix undefined

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const resolve = require('resolve-cwd');
 const pixie = require('pixie');
 
 function help(bot, config) {
-  const scripts = config.plugins;
   const prefix = config.prefix;
-  scripts.splice(config.plugins.indexOf('cordlr-help'), 1);
-  config = config[help.name] || {};
-  const format = config.help || 'Command: {{command}}\n\tUsage: {{prefix}}{{usage}}';
-  const scope = config.scope || ['channel'];
-  const unknown = config.unknown || 'Command {{command}} does not exist';
-  const plugins = scripts.map(p => require(resolve(p)));
-  const commands = plugins.filter(p => {
-    if (!(p.command && p.usage)) p(bot, config);
-    return p.command && p.usage;
-  });
-  commands.push({
-    command: 'help',
-    usage: 'help [command]'
-  });
-  const helpMessage = commands.map(c => {
-    return pixie.render(format, Object.assign({
-      prefix: prefix
-    }, c));
-  });
+  config.plugins.splice(config.plugins.indexOf('cordlr-help'), 1);
+  const helpConfig = config[help.name] || {};
+  const format = helpConfig.help || 'Command: {{command}}\n\tUsage: {{prefix}}{{usage}}';
+  const scope = helpConfig.scope || ['channel'];
+  const unknown = helpConfig.unknown || 'Command {{command}} does not exist';
 
   return function run(message, args) {
+    const plugins = config.plugins.map(p => require(resolve(p)));
+    const commands = plugins.filter(p => {
+      if (!(p.command && p.usage)) p(bot, config);
+      return p.command && p.usage;
+    });
+    commands.push({
+      command: 'help',
+      usage: 'help [command]'
+    });
+    const helpMessage = commands.map(c => {
+      return pixie.render(format, Object.assign({
+        prefix: prefix
+      }, c));
+    });
+    
     if (args.length > 0) {
       const command = args[0];
       const commandHelp = commands.find(c => c.name === command);
